533d3bc38096df46310000f3d7a904e168d315a4
kokobkidu <kokobkidu5@gmail.com>
2026-07-14 13:23:07 +0300

    Fetch football fixtures in home route
    Updated the home route to fetch and return football fixture data from an external API.

--- api/index.py
+++ api/index.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Koki Score API በስኬት እየሰራ ነው!"})
+    # የኳስ መረጃውን የሚያመጣው API
+    url = "https://v3.football.api-sports.io/fixtures?league=39&season=2023&last=5"
+    headers = {
+        'x-rapidapi-host': 'v3.football.api-sports.io',
+        'x-rapidapi-key': 'e9a95b77eba7237f2a107a30efca4dbb'
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run()
