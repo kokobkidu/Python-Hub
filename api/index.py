b06b1fc8a67704627e16421dcaa8a9a751e0ccd0
kokobkidu <kokobkidu5@gmail.com>
2026-07-17 13:07:17 +0300

    Fix URL assignment and error handling in home route

--- api/index.py
+++ api/index.py
@@ -5,14 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-url = "[https://v3.football.api-sports.io/fixtures?league=39&season=2023"
+    url = "https://v3.football.api-sports.io/fixtures?league=39&season=2023"
     headers = {
         'x-rapidapi-host': 'v3.football.api-sports.io',
         'x-rapidapi-key': 'e9a95b775eba7237f2a107a30bad87f9'
     }
     try:
         response = requests.get(url, headers=headers)
-        # ኮዱ እዚህ ጋር ነው የሚስተካከለው
         return jsonify(response.json())
     except Exception as e:
         return jsonify({"error": str(e)})
